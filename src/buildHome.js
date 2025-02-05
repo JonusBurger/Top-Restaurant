ce5abfbb92a8c4bd901fceaa6d209f82606ba03e
JonusBurger <j.bergner@live.de>
2025-02-05 06:39:33 +0100

    finish homePage, start implementation of buildPage

--- src/buildHome.js
+++ src/buildHome.js
@@ -23,4 +23,6 @@ export default function buildHomePage() {
         cellDiv.classList.add("cell");
         mainGridDiv.appendChild(cellDiv);
     }
+    mainDiv.appendChild(bannerDiv);
+    mainDiv.appendChild(mainGridDiv);
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import "./styles.css";
 import buildHomePage from "./buildHome.js"
+
 console.log("Moin Maista");
+
+function buildPage() {
+    const mainDiv = document.getElementById("content");
+
+    function emptyPage() {
+        while (mainDiv.firstChild) {
+            mainDiv.remove(mainDiv.lastChild);
+        }
+    }
+
+    return { emptyPage }
+} 
+
 buildHomePage();
+
+// Make buildPage have a default state
+
+// create EventListeners for Buttons
+
+// start building each page content
